c659eb4a1388c03f37b0f2f9b3295e8684b01b04
Farzad FARID <farzy@farzy.org>
2023-09-08 15:39:35 +0200

    Finish app

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,11 +1,15 @@
 use std::io;
+use std::io::Write;
 
 fn main() {
+    print!("Enter your weight (kg): ");
+    io::stdout().flush().unwrap();
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input);
-    println!("Input: {}", input);
-    let mut mars_weight = calculate_weight_on_mars(100.0);
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().expect("expected a number");
+
+    let mut mars_weight = calculate_weight_on_mars(weight);
     println!("Weight on Mars: {}kg", mars_weight); // Use "cargo expand" to expand the macro
 }
 
