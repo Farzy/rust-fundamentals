88b429a3261564adf6b95e6e312875833a3adfcf
Farzad FARID <farzy@farzy.org>
2023-09-09 15:38:57 +0200

    Parse a Method from a string

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,4 +1,4 @@
-use super::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -36,6 +36,8 @@ impl TryFrom<&[u8]> for Request {
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
+
+        let method: Method = method.parse()?;
 
         todo!()
     }
@@ -81,6 +83,12 @@ impl ParseError {
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
