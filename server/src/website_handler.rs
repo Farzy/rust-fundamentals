1dc1b65155fc0f65e5a0f37417c9ef6904745347
Farzad FARID <farzy@farzy.org>
2023-09-11 11:50:37 +0200

    Routing incoming requests

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,4 +1,4 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
 
 pub struct WebsiteHandler;
@@ -6,6 +6,14 @@ pub struct WebsiteHandler;
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         dbg!(request);
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+        // Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, Some(String::from("Not Found\r\n"))),
+            }
+            _ => Response::new(StatusCode::NotFound, Some(String::from("Not Found\r\n"))),
+        }
     }
 }
