522771fc7c2da162eea5c0def40be164277d71fe
Farzad FARID <farzy@farzy.org>
2023-09-11 13:31:17 +0200

    Serving arbitrary files securely

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
 
@@ -9,6 +10,21 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal attack attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
 }
 
 impl Handler for WebsiteHandler {
@@ -17,9 +33,12 @@ impl Handler for WebsiteHandler {
         // Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, Some(String::from("Not Found\r\n"))),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, Some(String::from("Not Found\r\n"))),
+                }
             }
             _ => Response::new(StatusCode::NotFound, Some(String::from("Not Found\r\n"))),
         }
