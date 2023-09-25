151db7122712a0992a7f794accaddc05486747ab
Farzad FARID <farzy@farzy.org>
2023-09-25 13:43:31 +0200

    Add parent field with Weak<Node> type, show that there is no circulal reference when printing the variable

--- playground2/src/main.rs
+++ playground2/src/main.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use crate::List::*;
 
 #[derive(Debug)]
@@ -49,16 +49,23 @@ fn circular_reference() {
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
 fn weak_reference() {
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
+    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 0,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
 }
