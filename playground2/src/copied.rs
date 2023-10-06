73665103ce0265ee6e41d151adb8c5358e34614b
Farzad FARID <farzy@farzy.org>
2023-10-06 19:21:44 +0200

    Reformat code

--- playground2/src/copied.rs
+++ playground2/src/copied.rs
@@ -5,10 +5,16 @@ pub fn main() {
     // Bad usage, without "copied()", on purpose
     let v: Vec<_> = a.iter().collect();
 
-    #[allow(clippy::iter_cloned_collect)] let v_copied: Vec<_> = a.iter().copied().collect();
+    #[allow(clippy::iter_cloned_collect)] let v_copied: Vec<_> = a
+        .iter()
+        .copied()
+        .collect();
 
     // copied is the same as .map(|&x| x)
-    #[allow(clippy::map_clone)] let v_map: Vec<_> = a.iter().map(|&x| x).collect();
+    #[allow(clippy::map_clone)] let v_map: Vec<_> = a
+        .iter()
+        .map(|&x| x)
+        .collect();
 
     assert_eq!(v, vec![&"a", &"b", &"c"]);
     assert_eq!(v_copied, vec!["a", "b", "c"]);
